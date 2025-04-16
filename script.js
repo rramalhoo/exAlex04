492057aca5fb35f0af2c73d251ff2c77ce5498e9
Ramalho <159273388+rramalhoo@users.noreply.github.com>
2025-04-16 08:01:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function comparar() {
+    let precoEtanol = parseFloat(document.getElementById("precoEtanol").value)
+    let precoGasolina = parseFloat(document.getElementById("precoGasolina").value)
+    let kmEtanol = parseFloat(document.getElementById("eficienciaEtanol").value)
+    let kmGasolina = parseFloat(document.getElementById("eficienciaGasolina").value)
+
+}

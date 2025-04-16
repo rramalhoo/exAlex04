1c6a68902b1951e0012674e28e27ae69312c5777
Ramalho <159273388+rramalhoo@users.noreply.github.com>
2025-04-16 08:02:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ function comparar() {
     let precoGasolina = parseFloat(document.getElementById("precoGasolina").value)
     let kmEtanol = parseFloat(document.getElementById("eficienciaEtanol").value)
     let kmGasolina = parseFloat(document.getElementById("eficienciaGasolina").value)
+    let valorPorKmEtanol = precoEtanol / kmEtanol
+    let valorPorKmGasolina = precoGasolina / kmGasolina
+    let vantagem = ''
+    let msg = ''
 
+
+    if (valorPorKmEtanol < valorPorKmGasolina) {
+
+
+    }
 }

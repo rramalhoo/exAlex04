d204298e7d28f56efb502486299a2cc82285cd3e
rapharamalho <raphael.athenesi@gmail.com>
2025-04-17 00:22:20 -0300

    final commmit

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 function comparar() {
-    let precoEtanol = parseFloat(document.getElementById("precoEtanol").value)
-    let precoGasolina = parseFloat(document.getElementById("precoGasolina").value)
-    let kmEtanol = parseFloat(document.getElementById("eficienciaEtanol").value)
-    let kmGasolina = parseFloat(document.getElementById("eficienciaGasolina").value)
-    let valorPorKmEtanol = precoEtanol / kmEtanol
-    let valorPorKmGasolina = precoGasolina / kmGasolina
-    let vantagem = ''
-    let msg = ''
-
-
-    if (valorPorKmEtanol < valorPorKmGasolina) {
-
-
+    var precoEtanol = parseFloat(document.getElementById("precoEtanol").value)
+    var precoGasolina = parseFloat(document.getElementById("precoGasolina").value)
+    var kmEtanol = parseFloat(document.getElementById("eficienciaEtanol").value)
+    var kmGasolina = parseFloat(document.getElementById("eficienciaGasolina").value)
+    var custoPorKmEtanol = precoEtanol / kmEtanol
+    var custoPorKmGasolina = precoGasolina / kmGasolina 
+    var empate = ''
+    var vantagem = ''
+    
+    
+    if (custoPorKmEtanol < custoPorKmGasolina) {
+        vantagem = 'O combustivel mais vantajoso é o <strong>Etanol</strong>'
     }
+    if (custoPorKmEtanol > custoPorKmGasolina) {
+        vantagem = 'O combustivel mais vantajoso é a <strong>Gasolina</strong>'
+    }
+    if(custoPorKmEtanol === custoPorKmGasolina){
+        empate = 'Os dois combustiveis são <strong>vantajosos</strong>'
+    }
+    
+
+    resposta = vantagem + '<br>' +empate+ '<br>' + '<br>' + 'O custo por KM do Etanol é ' + custoPorKmEtanol.toFixed(2) + '<br>' + 'O custo por KM da Gasolina é ' + custoPorKmGasolina.toFixed(2)
+    
+    document.getElementById("resposta").innerHTML = resposta
 }
